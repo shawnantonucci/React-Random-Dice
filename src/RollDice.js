6b7c864c34baa86242df324fdb24f96059456a47
shawn <shawnantonucci2018@gmail.com>
2019-05-25 10:10:43 -0700

    animation and button styles

--- src/RollDice.js
+++ src/RollDice.js
@@ -10,7 +10,8 @@ class RollDice extends Component {
     super(props);
     this.state = {
       die1: "one",
-      die2: "two"
+      die2: "two",
+      rolling: false
     };
   }
   roll = () => {
@@ -20,16 +21,21 @@ class RollDice extends Component {
     const newDie2 = this.props.sides[
       Math.floor(Math.random() * this.props.sides.length)
     ];
-    this.setState({ die1: newDie1, die2: newDie2 });
+    this.setState({ die1: newDie1, die2: newDie2, rolling: true });
+    setTimeout(() => {
+      this.setState({ rolling: false });
+    }, 1000);
   };
   render() {
     return (
       <div className="RollDice">
         <div className="RollDice-container">
-          <Die face={this.state.die1} />
-          <Die face={this.state.die2} />
+          <Die face={this.state.die1} rolling={this.state.rolling} />
+          <Die face={this.state.die2} rolling={this.state.rolling} />
         </div>
-        <button onClick={this.roll}>Roll Dice!</button>
+        <button onClick={this.roll} disabled={this.state.rolling}>
+          {this.state.rolling ? "Rolling..." : "Roll Dice!"}
+        </button>
       </div>
     );
   }
